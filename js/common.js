826aa93a28cf5871a022f5d7c3668dd6dd86d886
tomsuzuki <tomsuzuki2633@gmail.com>
2024-03-23 00:19:56 +0900

    [add] color

---
+++ js/common.js
@@ -0,0 +1,31 @@
+
+// loadTextFile ...テキストファイルをロードします。
+function loadTextFile(fName, Callback) {
+    var httpObj = createXMLHttpRequest();
+    httpObj.onreadystatechange = function () {
+        if (httpObj.readyState === 4 && httpObj.status === 200) {
+            Callback(httpObj.responseText);
+        }
+    }
+    httpObj.open("GET", fName, true);
+    httpObj.send(null);
+}
+
+// createXMLHttpRequest ...XMLHttpRequestを作成します。
+function createXMLHttpRequest() {
+    var XMLhttpObject = null;
+    try {
+        XMLhttpObject = new XMLHttpRequest();
+    } catch (e) {
+        try {
+            XMLhttpObject = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                XMLhttpObject = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                return null;
+            }
+        }
+    }
+    return XMLhttpObject;
+}
---
+++ js/main.js
@@ -0,0 +1,84 @@
+
+// 読み込み処理
+window.addEventListener("load", async function () {
+    // 色ファイルを取得する
+    var colors = await new Promise(r => loadTextFile("./data/color.json", (text) => { r(JSON.parse(text)) }));
+
+    // HTMLをリセット
+    var content = document.querySelector("#content");
+    while (content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+
+    // 生成
+    for (var i in colors["list"]) {
+        var color_code_tag = `color_code_${i}`;
+
+        var color = colors["list"][i];
+        var pallet = document.createElement("div");
+        pallet.classList.add("pallet");
+
+        for(var c of color["color"]) {
+            var pf = document.createElement("div");
+            var pa = document.createElement("div");
+            pf.classList.add("paint_frame");
+            pa.classList.add("paint");
+            pa.style.backgroundColor = c;
+            pf.setAttribute("onclick", `changeColorCode("${color_code_tag}", "${c}");`);
+            pf.appendChild(pa);
+            pallet.appendChild(pf);
+        }
+
+        var frame = document.createElement("div");
+        frame.classList.add("frame");
+
+        var title_frame = document.createElement("div");
+        title_frame.classList.add("title_frame");
+
+        var title = document.createElement("div");
+        title.classList.add("title");
+        title.innerText = color["title"];
+        title.style.background = `linear-gradient(transparent 80%, ${color["color"][0]} 60%)`;
+
+        var color_code = document.createElement("div");
+        color_code.classList.add("color_code");
+        color_code.id = color_code_tag;
+
+        var color_code_frame = document.createElement("div");
+        color_code_frame.classList.add("color_code_frame");
+
+        title_frame.appendChild(title);
+        color_code_frame.appendChild(color_code);
+
+        frame.appendChild(title_frame);
+        frame.appendChild(pallet);
+        frame.appendChild(color_code_frame);
+
+        content.appendChild(frame);
+
+        changeColorCode(color_code_tag, color["color"][0]);
+    }
+
+});
+
+// カラーコード変更
+function changeColorCode(id, color) {
+    var elem = document.querySelector(`#${id}`);
+    elem.innerHTML = color.toUpperCase();
+    elem.style.backgroundColor = color;
+    elem.style.color = getTextColor(color);
+}
+
+// 文字色を生成（白 or 黒）
+function getTextColor(rgb) {
+    let r = parseInt("0x" + rgb.substr(1, 2), 16);
+    let g = parseInt("0x" + rgb.substr(3, 2), 16);
+    let b = parseInt("0x" + rgb.substr(5, 2), 16);
+    let bk = Math.sqrt(
+      Math.pow(r - 0, 2) + Math.pow(g - 0, 2) + Math.pow(b - 0, 2)
+    );
+    let wh = Math.sqrt(
+      Math.pow(r - 255, 2) + Math.pow(g - 255, 2) + Math.pow(b - 255, 2)
+    );
+    return bk > wh ? "#000000" : "#FFFFFF";
+}
